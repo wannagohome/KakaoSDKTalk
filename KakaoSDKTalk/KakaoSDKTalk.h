1eae69c8869fdb41e368ecdb23b02495312311df
wannagohome <iwwuf7@gmail.com>
2020-09-24 16:15:01 +0900

    Initial Commit

---
+++ KakaoSDKTalk/KakaoSDKTalk.h
@@ -0,0 +1,19 @@
+//
+//  KakaoSDKTalk.h
+//  KakaoSDKTalk
+//
+//  Created by jinho jang on 2020/09/24.
+//  Copyright © 2020 People. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KakaoSDKTalk.
+FOUNDATION_EXPORT double KakaoSDKTalkVersionNumber;
+
+//! Project version string for KakaoSDKTalk.
+FOUNDATION_EXPORT const unsigned char KakaoSDKTalkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KakaoSDKTalk/PublicHeader.h>
+
+
